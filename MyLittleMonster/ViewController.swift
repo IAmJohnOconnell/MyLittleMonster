d42e3d614a131d38e6a2a64bf446b6a457d5ced0
John OConnell <john.oconnell@verizonwireless.com>
2016-01-03 19:10:51 -0500

    Initial Commit

---
+++ MyLittleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLittleMonster
+//
+//  Created by John Oconnell on 1/3/16.
+//  Copyright © 2016 JTOSoftworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
